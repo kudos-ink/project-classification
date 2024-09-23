3593b26541db66a417d4f29eb0af79563414914a
Connor Campbell <ccampbell.james@gmail.com>
2024-09-23 08:55:47 +0100

    reexport helpers

--- src/shared/src/lib.rs
+++ src/shared/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod functions;
 pub mod types;
 
-pub use functions::{extract_project, import_repositories, insert_project};
+pub use functions::{
+    extract_issue, extract_project, get_username_map, import_repositories, insert_project,
+};
 pub use types::{Payload, Project, ProjectAttributes, ProjectLinks, RepoInfo, Repository};
